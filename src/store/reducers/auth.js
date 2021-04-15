fbf4c55e0a3d182037e277b5b4519c7ce06f4fb4
Lester016 <lesterdeguzman1999@gmail.com>
2021-04-15 14:59:47 +0800

    Remove error message when authenticated

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -24,6 +24,7 @@ const reducer = (state = initialState, action) => {
         email: action.email,
         name: action.name,
         loading: false,
+        error: null,
       };
     case actionTypes.AUTH_FAILED:
       return {
