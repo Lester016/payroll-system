f7d08b82239528b2a3e62851f5fce3f218114932
captainalbert <49594557+captainalbert@users.noreply.github.com>
2021-05-16 01:50:55 +0800

    Modify: aligned the pdf layout with TUP payslip.

--- src/containers/Payroll.js
+++ src/containers/Payroll.js
@@ -25,7 +25,6 @@ function Payroll() {
     {
       id: "employeeName",
       label: "Name",
-      disableSorting: true,
     },
     {
       id: "position",
@@ -56,97 +55,129 @@ function Payroll() {
       employeeID: "123",
       employeeName: "Nikko Cruz",
       position: "Janitor",
-      grossPay: 1000,
-      netPay: 1650,
+      grossPay: 15000,
+      netPay: 13050,
       deduction: 500,
     },
     employee2: {
       employeeID: "234",
       employeeName: "Mark Dela Fuente",
       position: "Professor",
-      grossPay: 1100,
-      netPay: 1120,
+      grossPay: 10100,
+      netPay: 9120,
       deduction: 650,
     },
     employee3: {
       employeeID: "345",
       employeeName: "Juan Dela Cruz",
       position: "Admin",
-      grossPay: 1200,
-      netPay: 1050,
+      grossPay: 15200,
+      netPay: 13550,
       deduction: 350,
     },
     employee4: {
       employeeID: "456",
       employeeName: "Mary Anne Santos",
       position: "Dean",
-      grossPay: 1300,
-      netPay: 1651,
+      grossPay: 16300,
+      netPay: 15500,
       deduction: 486,
     },
     employee5: {
       employeeID: "567",
       employeeName: "Jude Mamamia",
       position: "Staff",
-      grossPay: 1400,
-      netPay: 1136,
+      grossPay: 14000,
+      netPay: 13136,
       deduction: 320,
     },
     employee6: {
       employeeID: "678",
       employeeName: "James Lebron",
       position: "Profssor2",
-      grossPay: 1500,
-      netPay: 2100,
+      grossPay: 13500,
+      netPay: 12100,
       deduction: 600,
     },
   };
 
   // HANDLES FOR PRINTABLES
-  const handlePayslip = () => {};
-
-  const handlePayroll = () => {
-    const pdf = new jsPDF("a4");
+  const handlePayslip = () => {
+    const pdf = new jsPDF("a6");
 
     const data = dummy;
 
+    let date = "JANUARY 1-31, 2021";
+
     // START OF PDF FILE
     // HEADER
-    pdf.setFontSize(16);
-    pdf.text("Technological University of the Philippines", 55, 10);
+    pdf.setFont("times", "bold");
     pdf.setFontSize(12);
-    pdf.text(
-      "1410, Ayala Blvd., cor. San Marcelino St. ermita, Manila 1000",
-      50,
-      18
-    );
-    pdf.text("Contact: (+63)-253-01-3-001  Email: tup@tup.edu.ph", 60, 26);
-    pdf.line(20, 33, 190, 33);
+    pdf.text("TUP MANILA", 90, 10);
+    pdf.text("PAYROLL PAYMENT SLIP", 75, 15);
+    pdf.text(`For the period of ${date}`, 70, 20);
+    pdf.setFontSize(12);
+    pdf.setFont("times", "normal");
 
     // BODY
     pdf.setFontSize(10);
 
-    // DATE
-    pdf.text("Date: April 10, 2021 - May 10, 2021 ", 130, 40);
-
     // RENDERS EMPLOYEE ID
-    pdf.text("Employee ID:", 25, 50);
-    pdf.text(data.employee1.employeeID, 55, 50);
+    pdf.text("Employee No.", 10, 50);
+    pdf.setFont("times", "bold");
+    pdf.text(data.employee1.employeeID, 40, 50);
+    pdf.setFont("times", "normal");
+
+    // RENDERS POSITION
+    pdf.text("Position: ", 100, 50);
+    pdf.setFont("times", "bold");
+    pdf.text(data.employee1.position, 115, 50);
+    pdf.setFont("times", "normal");
 
     // RENDERS EMPLOYEE NAME
-    pdf.text("Employee Name:", 25, 60);
-    pdf.text(data.employee1.employeeName, 55, 60);
+    pdf.text("Employee Name:", 10, 55);
+    pdf.setFont("times", "bold");
+    pdf.text(data.employee1.employeeName, 40, 55);
+    pdf.setFont("times", "normal");
 
-    // RENDERS EMPLOYEE NETPAY
-    pdf.text("Net Pay:", 25, 70);
-    pdf.text(`P${data.employee1.netPay}.00`, 40, 70);
+    // RENDERS GROSS PAY
+    pdf.text("Gross Amount Due ", 10, 70);
 
-    // RENDERS TOTAL PAY
-    pdf.setFontSize(20);
-    pdf.text(`Total: P${data.employee1.netPay}.00`, 130, 80);
+    pdf.line(175, 65, 200, 65);
+    pdf.setFont("times", "bold");
+    pdf.text(`${data.employee1.grossPay}`, 185, 70); //Int values needs to be renders as string in jsPDF
+    pdf.setFont("times", "normal");
+    pdf.line(175, 73, 200, 73);
+
+    // RENDERS DEDUCTION
+    pdf.text("Total Deduction ", 140, 83);
+    pdf.setFont("times", "bold");
+    pdf.text(`${data.employee1.deduction}`, 185, 83); //Int values needs to be renders as string in jsPDF
+    pdf.setFont("times", "normal");
+
+    pdf.line(140, 85, 200, 85);
+
+    // RENDERS NET PAY
+    pdf.text("Net Amount ", 140, 90);
+    pdf.setFont("times", "bold");
+    pdf.text(`${data.employee1.netPay}`, 185, 90); //Int values needs to be renders as string in jsPDF
+    pdf.setFont("times", "normal");
+
+    // FOOTER
+    pdf.setFontSize(12);
+    pdf.setFont("times", "bold");
+
+    pdf.text("Prepared by: ", 10, 105);
+    pdf.text("Certified correct: ", 100, 105);
+
+    pdf.text("CATALINA M. BAQUIRAN ", 40, 125);
+    pdf.text("Administrative Officer IV ", 43, 130);
+
+    pdf.text("ATTY. CHRISTOPHER M. MORTEL ", 130, 125);
+    pdf.text("Head, HRMS ", 155, 130);
 
     // END OF PDF FILE
-    pdf.save("payroll");
+    pdf.save("payroll"); //Prints the pdf
   };
 
   return (
@@ -159,7 +190,6 @@ function Payroll() {
           propertiesOrder={columnHeads.slice(0, 6).map((item) => item.id)}
           isPayroll={true}
           printPayslip={handlePayslip} //Generate PDF functions
-          printPayroll={handlePayroll} //Generate PDF functions
         />
       </Paper>
     </div>
