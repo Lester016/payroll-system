27f090b85f5b10f7b7552890bb2ae54362042862
captainalbert <49594557+captainalbert@users.noreply.github.com>
2021-05-21 00:52:46 +0800

    Modify: Changed and added some charts

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,35 +1,120 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 
-import ComputerIcon from "@material-ui/icons/Computer";
-import PaletteIcon from "@material-ui/icons/Palette";
-import SettingsIcon from "@material-ui/icons/Settings";
-import WorkIcon from "@material-ui/icons/Work";
-import BuildIcon from "@material-ui/icons/Build";
-import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import { Doughnut, Bar } from "react-chartjs-2";
 
-import { Doughnut } from "react-chartjs-2";
-
-const count = [
-  { campus: "TUP-Manila", employees: 250 },
-  { campus: "TUP-Taguig", employees: 130 },
-  { campus: "TUP-Cavite", employees: 150 },
-  { campus: "TUP-Visayas", employees: 120 },
+// GENDER SET UP FOR GENDER CHART
+const gender = [
+  { sex: "Male", count: 1560 },
+  { sex: "Female", count: 1989 },
 ];
 
-const chartData = {
+const genderData = {
   responsive: true,
-  labels: [count[0].campus, count[1].campus, count[2].campus, count[3].campus],
+  labels: [gender[0].sex, gender[1].sex],
+  datasets: [
+    {
+      data: [gender[0].count, gender[1].count],
+      backgroundColor: ["rgb(54, 162, 235)", "rgb(255, 99, 132)"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const genderConfig = {
+  plugins: {
+    title: {
+      display: true,
+      text: "GENDER",
+      font: {
+        size: 20,
+      },
+    },
+    legend: {
+      labels: {
+        boxHeight: 20,
+        boxWidth: 25,
+      },
+      position: "bottom",
+    },
+  },
+};
+
+// POSITION SET UP FOR POSITION CHART
+const position = [
+  { jobRole: "Dean", employeeCount: 80 },
+  { jobRole: "Admin", employeeCount: 500 },
+  { jobRole: "Teacher", employeeCount: 1500 },
+  { jobRole: "Cleaner", employeeCount: 300 },
+];
+
+const positionData = {
+  responsive: true,
+  labels: [
+    position[0].jobRole,
+    position[1].jobRole,
+    position[2].jobRole,
+    position[3].jobRole,
+  ],
   datasets: [
     {
       data: [
-        count[0].employees,
-        count[1].employees,
-        count[2].employees,
-        count[3].employees,
+        position[0].employeeCount,
+        position[1].employeeCount,
+        position[2].employeeCount,
+        position[3].employeeCount,
+      ],
+      backgroundColor: ["red", "blue", "green", "gray"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const positionConfig = {
+  plugins: {
+    title: {
+      display: true,
+      text: "POSITIONS",
+      font: {
+        size: 20,
+      },
+    },
+    legend: {
+      labels: {
+        boxHeight: 20,
+        boxWidth: 25,
+      },
+      position: "bottom",
+    },
+  },
+};
+
+// BUDGET SET UP FOR BUDGET CHART
+const budget = [
+  { campus: "TUP-Manila", payroll: 3500200 },
+  { campus: "TUP-Taguig", payroll: 2600600 },
+  { campus: "TUP-Cavite", payroll: 2500600 },
+  { campus: "TUP-Visayas", payroll: 1700520 },
+];
+
+const budgetData = {
+  responsive: true,
+  labels: [
+    budget[0].campus,
+    budget[1].campus,
+    budget[2].campus,
+    budget[3].campus,
+  ],
+  datasets: [
+    {
+      data: [
+        budget[0].payroll,
+        budget[1].payroll,
+        budget[2].payroll,
+        budget[3].payroll,
       ],
       backgroundColor: [
         "rgb(255, 99, 132)",
@@ -42,22 +127,62 @@ const chartData = {
   ],
 };
 
-const options = {
+const budgetConfig = {
   plugins: {
     title: {
       display: true,
-      text: "EMPLOYEE PERCENTAGE OF TUP CAMPUSES",
+      text: "PAYROLL BUDGET FOR TUP CAMPUSES",
       font: {
-        size: 30,
+        size: 20,
       },
-      color: "green",
     },
     legend: {
       labels: {
-        boxHeight: 30,
-        boxWidth: 35,
+        boxHeight: 20,
+        boxWidth: 25,
       },
-      position: "right",
+      position: "bottom",
+    },
+  },
+};
+
+// COLLEGE SET UP FOR COLLEGE CHART
+const college = [
+  { department: "College of Science", employees: 350 },
+  { department: "College of Architecture and Fine Arts", employees: 100 },
+  { department: "College of Liberal Arts", employees: 120 },
+  { department: "College of Industrial Technology", employees: 100 },
+  { department: "College of Engineering", employees: 150 },
+  { department: "College of Industrial Education", employees: 120 },
+];
+
+const collegeData = {
+  labels: ["COS", "CAFA", "CLA", "CIT", "COE", "CIE"],
+  datasets: [
+    {
+      data: [
+        college[0].employees,
+        college[1].employees,
+        college[2].employees,
+        college[3].employees,
+        college[4].employees,
+        college[5].employees,
+      ],
+      backgroundColor: ["red", "green", "blue", "orange", "yellow", "gray"],
+      borderColor: ["red", "green", "blue", "orange", "yellow", "gray"],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const collegeConfig = {
+  plugins: {
+    title: {
+      display: true,
+      text: "EMPLOYEE DISTRIBUTION",
+      font: {
+        size: 20,
+      },
     },
   },
 };
@@ -71,75 +196,22 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 60,
     color: "white",
     fontWeight: "bold",
-
-    // backgroundColor: "green",
   },
+
   cardDescription: {
     fontSize: 15,
     color: "white",
-    // backgroundColor: "blue",
   },
 
-  cos: {
-    display: "flex",
-    padding: theme.spacing(2),
+  chart: {
+    padding: theme.spacing(3),
     color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#D44949",
-  },
-
-  cafa: {
-    display: "flex",
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#D0B72B",
-  },
-  cla: {
-    display: "flex",
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#4384E4",
-  },
-  cit: {
-    display: "flex",
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#FF821C",
-  },
-
-  coe: {
-    display: "flex",
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#21BA21",
-  },
-
-  cie: {
-    display: "flex",
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    height: 200,
-
-    backgroundColor: "#D638D8",
-  },
-
-  chartCard: {
-    padding: theme.spacing(2),
-    backgroundColor: "#F7F2F2",
   },
 
   left: {
     flex: 2,
   },
+
   icon: {
     fontSize: 100,
   },
@@ -148,138 +220,43 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
   const classes = useStyles();
 
-  const college = [
-    { department: "College of Science", employees: 350 },
-    { department: "College of Architecture and Fine Arts", employees: 100 },
-    { department: "College of Liberal Arts", employees: 120 },
-    { department: "College of Industrial Technology", employees: 100 },
-    { department: "College of Engineering", employees: 150 },
-    { department: "College of Industrial Education", employees: 120 },
-  ];
-
   return (
     <div className={classes.root}>
       <h1>Welcome, Admin!</h1>
       <Grid container spacing={3}>
-        {/* COS */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.cos}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[0].employees}
-              </Typography>
-              <Typography className={classes.cardDescription}>
-                {college[0].department}
-              </Typography>
+        {/* GENDER CHART */}
+        <Grid item xs={12} xs={12} sm={10} md={4}>
+          <Card className={classes.chart}>
+            <div className={classes.left} style={{ position: "relative" }}>
+              <Doughnut data={genderData} options={genderConfig} />
             </div>
-            <ComputerIcon className={classes.icon} />
           </Card>
         </Grid>
 
-        {/* CAFA */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.cafa}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[1].employees}
-              </Typography>
-              <Typography className={classes.cardDescription}>
-                {college[1].department}
-              </Typography>
+        {/* PAYROLL CHART */}
+        <Grid item xs={12} xs={12} sm={10} md={4}>
+          <Card className={classes.chart}>
+            <div className={classes.left} style={{ position: "relative" }}>
+              <Doughnut data={budgetData} options={budgetConfig} />
             </div>
-            <PaletteIcon className={classes.icon} />
           </Card>
         </Grid>
 
-        {/* CLA */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.cla}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[2].employees}
-              </Typography>
-              <Typography className={classes.cardDescription}>
-                {college[2].department}
-              </Typography>
+        {/* POSITION CHART */}
+        <Grid item xs={12} xs={12} sm={10} md={4}>
+          <Card className={classes.chart}>
+            <div className={classes.left} style={{ position: "relative" }}>
+              <Doughnut data={positionData} options={positionConfig} />
             </div>
-            <WorkIcon className={classes.icon} />
           </Card>
         </Grid>
-
-        {/* CIT */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.cit}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[3].employees}
-              </Typography>
-              <Typography className={classes.cardDescription}>
-                {college[3].department}
-              </Typography>
-            </div>
-            <BuildIcon className={classes.icon} />
-          </Card>
-        </Grid>
-
-        {/* COE */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.coe}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[4].employees}
-              </Typography>
-
-              <Typography className={classes.cardDescription}>
-                {college[4].department}
-              </Typography>
-            </div>
-            <SettingsIcon className={classes.icon} />
-          </Card>
-        </Grid>
-
-        {/* CIE */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.cie}>
-            <div className={classes.left}>
-              <Typography className={classes.cardDescription}>
-                Employees
-              </Typography>
-              <Typography className={classes.cardTitle}>
-                {college[5].employees}
-              </Typography>
-              <Typography className={classes.cardDescription}>
-                {college[5].department}
-              </Typography>
-            </div>
-            <QuestionAnswerIcon className={classes.icon} />
-          </Card>
-        </Grid>
-
-        {/* CHART */}
-        <Grid item xs={12} lg={8}>
-          <Card className={classes.chartCard} style={{ position: "relative" }}>
-            <div
-              style={{
-                height: "80%",
-                width: "90%",
-              }}
-            >
-              <Doughnut data={chartData} options={options} />
+      </Grid>
+      <Grid container spacing={3}>
+        {/* BAR CHART */}
+        <Grid item xs={12} sm={10} md={12}>
+          <Card className={classes.chart}>
+            <div className={classes.left} style={{ position: "relative" }}>
+              <Bar data={collegeData} options={collegeConfig} />
             </div>
           </Card>
         </Grid>
