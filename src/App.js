d27d692c40bbf93fdf3a9670fbdeb7497a38cc0e
Lester016 <lesterdeguzman1999@gmail.com>
2021-05-23 07:54:57 +0800

    Create About and Help container

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { connect } from "react-redux";
 import { useEffect } from "react";
 
 import "./App.css";
+import * as actions from "./store/actions";
+import PrivateRoute from "./hoc/PrivateRoute";
+import Layout from "./hoc/Layout";
 import Deductions from "./containers/Deductions";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Positions from "./containers/Positions";
 import Payroll from "./containers/Payroll";
 import Employees from "./containers/Employees/Employees";
-import Layout from "./hoc/Layout";
-import PrivateRoute from "./hoc/PrivateRoute";
-import * as actions from "./store/actions";
 import Logout from "./containers/Logout";
+import Help from "./containers/Help";
+import About from "./containers/About";
 
 function App({ onAutoSignup }) {
   useEffect(() => {
@@ -24,6 +26,8 @@ function App({ onAutoSignup }) {
       <Route path="/login" exact component={Login} />
 
       <Layout>
+        <PrivateRoute path="/help" component={Help} />
+        <PrivateRoute path="/about" component={About} />
         <PrivateRoute path="/employees" component={Employees} />
         <PrivateRoute path="/payroll" component={Payroll} />
         <PrivateRoute path="/deductions" component={Deductions} />
