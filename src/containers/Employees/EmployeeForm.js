65c7bfe3af0c67be7af886900a10b97f8a515754
Jan Erickson Bataclan <jebataclan03@gmail.com>
2021-05-22 22:04:19 +0800

    Remove handleReset function from EmployeeForm because of uncontrolled components.

--- src/containers/Employees/EmployeeForm.js
+++ src/containers/Employees/EmployeeForm.js
@@ -275,10 +275,6 @@ const EmployeeForm = ({ handleFormClose }) => {
       }
     }
   };
-  const handleReset = () => {
-    setValues(initialValues);
-    setErrors({});
-  };
 
   const LoadingTUP = (
     <>
